70fec1281746226a66b8d770302c658980424c70
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2024-09-16 10:09:28 -0300

    feat: :sparkles: Listando os nomes dos filmes recuperados pela api

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState}  from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import Filmes from './src/Filmes';
+
+import api from './src/services/api';
 
 export default function App() {
+  const [filmes, setFilmes] = useState([]);
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get('r-api/?api=filmes')
+      setFilmes(response.data);
+    }
+    loadFilmes();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={filmes}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => <Filmes data={ item} />}
+      />
     </View>
   );
 }
