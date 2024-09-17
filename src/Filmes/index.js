b19e0229f420fd5deb4c8f54b4ef77af7afb12dc
Dhionathan lanzoni Jobim <dhionathan01@hotmail.com>
2024-09-17 10:53:16 -0300

    feat: :sparkles: Criando componentes filmes e detalhes e fazendo a listagem dos itens

--- src/Filmes/index.js
+++ src/Filmes/index.js
@@ -1,10 +1,59 @@
-import React from "react";
-import { View, Text } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal } from 'react-native';
+import Detalhes from "../Detalhes";
 
-export default function Filmes({data}) {
+export default function Filmes({ data }) {
+    const [visibleModal, setVisibleModal] = useState(false);
     return (
         <View>
-            <Text>{data.nome }</Text>
+            <View style={styles.card}>
+                <Text style={styles.titulo}>{data.nome}</Text>
+                <Image
+                    source={{ uri: data.foto }}
+                    style={styles.capa}
+                ></Image>
+                <View style={styles.areaBotao}>
+                    <TouchableOpacity style={styles.botao} onPress={()=>{setVisibleModal(true)}}>
+                        <Text style={styles.botaoTexto}>Leia mais</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <Modal transparent={true} animationType="slide" visible={visibleModal}>
+                <Detalhes filme={data} voltar={()=> setVisibleModal(false)} />
+            </Modal>
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: '#FFF',
+        margin: 15,
+        elevation: 3,
+    },
+    capa: {
+        height: 250,  
+        zIndex: 2,
+    },
+    titulo: {
+        padding: 15,
+        fontSize: 18,
+    },
+    areaBotao: {
+        alignItems: 'flex-end',
+        marginTop: -45,
+        zIndex: 3
+    },
+    botao: {
+        width: 100,
+        backgroundColor: '#09A6FF',
+        opacity: 1,
+        padding: 8,
+        borderTopLeftRadius: 5,
+        borderBottomLeftRadius: 5
+    },
+    botaoTexto: {
+        color: '#FFF',
+        textAlign: 'center'
+    }
+})
